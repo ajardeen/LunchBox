1d4eb21395b12d847809572e092fcb30ae7d263f
Mohamed Ajardeen <166899366+ajardeen@users.noreply.github.com>
2025-10-12 12:20:50 +0530

    update

--- src/layouts/Layouts.jsx
+++ src/layouts/Layouts.jsx
@@ -4,9 +4,9 @@ import TopNav from "./TopNav/TopNav";
 function Layouts({ children }) {
   return (
     <div className="flex flex-col min-h-screen w-full bg-white font-runde">
-      <main className="flex flex-col items-center flex-grow px-4 sm:px-6 md:px-12 lg:px-10">
+      <main className=" flex flex-col items-center flex-grow px-4 sm:px-6 md:px-12 lg:px-10">
         {/* Container with max width for large screens */}
-        <div className="w-full max-w-[1440px]">
+        <div className="border-2  w-full max-w-[1440px]">
           <TopNav />
           {children}
         </div>
